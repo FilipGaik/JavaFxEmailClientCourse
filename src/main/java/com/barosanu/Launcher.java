a108709837d70076c737ffab5641bd55a9fc0951
Filip Gaik <filipgaik@gmail.com>
2024-12-01 10:21:58 +0100

    Building the main window

--- src/main/java/com/barosanu/Launcher.java
+++ src/main/java/com/barosanu/Launcher.java
@@ -15,9 +15,9 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/LoginWindow.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/MainWindow.fxml"));
 
-        stage.setScene(new Scene(parent, 510, 325));
+        stage.setScene(new Scene(parent));
 
         stage.show();
     }
