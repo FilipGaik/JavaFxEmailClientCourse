570314146d36a98e3d086d523ed2f76286d7dc78
Filip Gaik <filipgaik@gmail.com>
2024-12-02 17:34:26 +0100

    Updating the CSS dynamically

--- src/main/java/com/barosanu/view/FontSize.java
+++ src/main/java/com/barosanu/view/FontSize.java
@@ -3,5 +3,18 @@ package com.barosanu.view;
 public enum FontSize {
     SMALL,
     MEDIUM,
-    BIG
+    BIG;
+
+    public static String getCssPath(FontSize fontSize) {
+        switch (fontSize) {
+            case MEDIUM:
+                return "/view/css/fontMedium.css";
+            case BIG:
+                return "/view/css/fontBig.css";
+            case SMALL:
+                return "/view/css/fontSmall.css";
+            default:
+                return null;
+        }
+    }
 }
