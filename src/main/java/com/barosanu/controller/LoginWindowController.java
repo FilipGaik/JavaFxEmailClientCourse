b3165ca133bb27eb543f8bdf1b4eb9af3b1464a9
Filip Gaik <filipgaik@gmail.com>
2024-12-03 17:02:20 +0100

    Adding extends Service to the LoginService

--- src/main/java/com/barosanu/controller/LoginWindowController.java
+++ src/main/java/com/barosanu/controller/LoginWindowController.java
@@ -31,18 +31,21 @@ public class LoginWindowController extends BaseController {
         if(fieldsAreValid()) {
             EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
             LoginService loginService = new LoginService(emailAccount, emailManager);
-            EmailLoginResult emailLoginResult = loginService.login();
+            loginService.start();
+            loginService.setOnSucceeded(event -> {
+                EmailLoginResult emailLoginResult = loginService.getValue();
 
-            switch (emailLoginResult) {
-                case SUCCESS:
-                    System.out.println("Login successfull!!! " + emailAccount);
-                    return;
-            }
+                switch (emailLoginResult) {
+                    case SUCCESS:
+                        System.out.println("Login successfull!!! " + emailAccount);
+                        viewFactory.showMainWindow();
+                        Stage stage = (Stage) errorLabel.getScene().getWindow();
+                        viewFactory.closeStage(stage);
+
+                        return;
+                }
+            });
         }
-
-        //viewFactory.showMainWindow();
-        //Stage stage = (Stage) errorLabel.getScene().getWindow();
-        //viewFactory.closeStage(stage);
     }
 
     private boolean fieldsAreValid() {
