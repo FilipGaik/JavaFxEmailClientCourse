ecdac0134aa294e7d76c12574b4b447bcc7e87f0
Filip Gaik <filipgaik@gmail.com>
2024-12-05 19:30:15 +0100

    Adding treeItems to folder structure

--- src/main/java/com/barosanu/EmailManager.java
+++ src/main/java/com/barosanu/EmailManager.java
@@ -1,4 +1,23 @@
 package com.barosanu;
 
+import com.barosanu.model.EmailAccount;
+import javafx.scene.control.TreeItem;
+
 public class EmailManager {
+
+    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+
+    public TreeItem<String> getFoldersRoot() {
+        return foldersRoot;
+    }
+
+    public void addEmailAccount(EmailAccount emailAccount) {
+        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
+        treeItem.setExpanded(true);
+        treeItem.getChildren().add(new TreeItem<String>("Inbox"));
+        treeItem.getChildren().add(new TreeItem<String>("Sent"));
+        treeItem.getChildren().add(new TreeItem<String>("Folder1"));
+        treeItem.getChildren().add(new TreeItem<String>("Spam"));
+        foldersRoot.getChildren().add(treeItem);
+    }
 }
