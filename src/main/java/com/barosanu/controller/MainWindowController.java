d1378967214d43cacc13942a0add114c277c9ea4
Filip Gaik <filipgaik@gmail.com>
2024-12-09 22:44:17 +0100

    TableView setup

--- src/main/java/com/barosanu/controller/MainWindowController.java
+++ src/main/java/com/barosanu/controller/MainWindowController.java
@@ -1,14 +1,18 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.model.EmailMessage;
+import com.barosanu.model.EmailTreeItem;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebView;
-
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
@@ -17,10 +21,25 @@ public class MainWindowController extends BaseController implements Initializabl
     private WebView emailWebView;
 
     @FXML
-    private TableView<?> emailsTableView;
+    private TableView<EmailMessage> emailsTableView;
 
     @FXML
     private TreeView<String> emailsTreeView;
+
+    @FXML
+    private TableColumn<EmailMessage, Date> dateCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> recipientCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> senderCol;
+
+    @FXML
+    private TableColumn<EmailMessage, Integer> sizeCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> subjectCol;
 
     public MainWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -39,6 +58,25 @@ public class MainWindowController extends BaseController implements Initializabl
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpEmailsTreeView();
+        setUpEmailsTableView();
+        setUpFolderSelection();
+    }
+
+    private void setUpFolderSelection() {
+        emailsTreeView.setOnMouseClicked(e -> {
+            EmailTreeItem<String> item = (EmailTreeItem<String>)emailsTreeView.getSelectionModel().getSelectedItem();
+            if(item != null) {
+                emailsTableView.setItems(item.getEmailMessages());
+            }
+        });
+    }
+
+    private void setUpEmailsTableView() {
+        senderCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("Sender"));
+        subjectCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("Subject"));
+        recipientCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("Recipient"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Integer>("Size"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Date>("Date"));
     }
 
     private void setUpEmailsTreeView() {
