13b123b2e28d4fb48b51f60f5d9040044be5b3d9
Filip Gaik <filipgaik@gmail.com>
2024-12-16 19:02:16 +0100

    Updating folders with new messages

--- src/main/java/com/barosanu/EmailManager.java
+++ src/main/java/com/barosanu/EmailManager.java
@@ -1,13 +1,27 @@
 package com.barosanu;
 
 import com.barosanu.controller.services.FetchFoldersService;
+import com.barosanu.controller.services.FolderUpdaterService;
 import com.barosanu.model.EmailAccount;
 import com.barosanu.model.EmailTreeItem;
-import javafx.scene.control.TreeItem;
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmailManager {
 
+    private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
+    private List<Folder> folderList = new ArrayList<Folder>();
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
 
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
@@ -15,7 +29,7 @@ public class EmailManager {
 
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
