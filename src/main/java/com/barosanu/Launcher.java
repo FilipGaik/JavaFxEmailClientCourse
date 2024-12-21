888edc43c592c3c77dd7324406eaaa374e3ce650
Filip Gaik <filipgaik@gmail.com>
2024-12-21 17:14:27 +0100

    Saving persisted data

--- src/main/java/com/barosanu/Launcher.java
+++ src/main/java/com/barosanu/Launcher.java
@@ -1,10 +1,19 @@
 package com.barosanu;
 
+import com.barosanu.controller.persistence.PersistenceAccess;
+import com.barosanu.controller.persistence.ValidAccount;
+import com.barosanu.controller.services.LoginService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Launcher extends Application {
+
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
 
     public static void main(String[] args) {
         launch(args);
@@ -13,7 +22,26 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if(!validAccountList.isEmpty()) {
+            viewFactory.showMainWindow();
+            for(ValidAccount validAccount : validAccountList) {
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
+    }
+
+    @Override
+    public void stop() {
+        List<ValidAccount> validAccountList = new ArrayList<>();
+        for(EmailAccount emailAccount : emailManager.getEmailAccounts()) {
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
     }
 }
--- src/main/java/com/barosanu/controller/persistence/PersistenceAccess.java
+++ src/main/java/com/barosanu/controller/persistence/PersistenceAccess.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class PersistenceAccess {
 
-    private String VALID_ACCOUNTS_LOCATION = System.getenv("APPDATA") + "\\validAccounts.ser";
+    private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "Desktop\\Filip\\Programowanie\\Projekt Przyszły Programista\\05. Technologie webowe\\14 JavaFX\\JavaFxEmailClientCourse\\validAccounts.ser";
 
     public List<ValidAccount> loadFromPersistence() {
         List<ValidAccount> resultList = new ArrayList<>();
