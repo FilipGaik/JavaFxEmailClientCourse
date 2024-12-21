8271c47962e9e332b8d36d9dbad16f228cf6e17c
Filip Gaik <filipgaik@gmail.com>
2024-12-21 14:45:33 +0100

    Custom Button class

--- src/main/java/com/barosanu/controller/EmailDetailsController.java
+++ src/main/java/com/barosanu/controller/EmailDetailsController.java
@@ -4,15 +4,18 @@ import com.barosanu.EmailManager;
 import com.barosanu.controller.services.MessageRendererService;
 import com.barosanu.model.EmailMessage;
 import com.barosanu.view.ViewFactory;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeBodyPart;
+import java.awt.Desktop;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -55,14 +58,67 @@ public class EmailDetailsController extends BaseController implements Initializa
         if(emailMessage.hasAttachments()) {
             for(MimeBodyPart mimeBodyPart : emailMessage.getAttachmentList()) {
                 try {
-                    Button button = new Button(mimeBodyPart.getFileName());
+                    AttachmentButton button = new AttachmentButton(mimeBodyPart);
                     hBoxDownloads.getChildren().add(button);
-                } catch(MessagingException e) {
+                } catch(Exception e) {
                     e.printStackTrace();
                 }
             }
         } else {
             attachmentLabel.setText("");
+        }
+    }
+
+    private class AttachmentButton extends Button {
+
+        private MimeBodyPart mimeBodyPart;
+        private String downloadedFilePath;
+
+        public AttachmentButton(MimeBodyPart mimeBodyPart) throws MessagingException {
+            this.mimeBodyPart = mimeBodyPart;
+            this.setText(mimeBodyPart.getFileName());
+            this.downloadedFilePath = LOCATION_OF_DOWNLOADS + mimeBodyPart.getFileName();
+
+            this.setOnAction(e -> downloadAttachment());
+        }
+
+        private void downloadAttachment() {
+            colorBlue();
+            Service service = new Service() {
+                @Override
+                protected Task createTask() {
+                    return new Task() {
+                        @Override
+                        protected Object call() throws Exception {
+                            mimeBodyPart.saveFile(downloadedFilePath);
+                            return null;
+                        }
+                    };
+                }
+            };
+            service.restart();
+            service.setOnSucceeded(e -> {
+                colorGreen();
+                this.setOnAction(e2 -> {
+                    File file = new File(downloadedFilePath);
+                    Desktop desktop = Desktop.getDesktop();
+                    if(file.exists()) {
+                        try {
+                            desktop.open(file);
+                        } catch(Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                });
+            });
+        }
+
+        private void colorBlue() {
+            this.setStyle("-fx-background-color: Blue");
+        }
+
+        private void colorGreen() {
+            this.setStyle("-fx-background-color: Green");
         }
     }
 }
